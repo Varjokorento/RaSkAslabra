3720b8bbd1023580dac10ff323a4502abac7f02e
Kalle Pusa <kalle.pusa@gmail.com>
2019-09-26 20:17:18 +0300

    Testejä

--- project/src/test/java/decrypterTest/DecrypterTest.java
+++ project/src/test/java/decrypterTest/DecrypterTest.java
@@ -3,13 +3,17 @@ package decrypterTest;
 import decrypter.Decrypter;
 import encrypter.Encrypter;
 import keygenerators.KeyUtil;
+import keyimpl.PrivateKeyImpl;
+import keyimpl.PublicKeyImpl;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.io.*;
+import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
@@ -28,11 +32,25 @@ public class DecrypterTest {
 
 
     @Test
-    public void decryptedStringIsDecryptedCorrectly() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException {
+    public void decryptedStringIsDecryptedCorrectlyWithLibraries() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException {
         PublicKey publicKey = KeyUtil.readPublicKeyFile("src/test/resources/publicKey.pub");
         PrivateKey privateKey = KeyUtil.readPrivateKeyFile("src/test/resources/privateKey.key");
         byte[] encryptedByteArray = encrypter.encrypt("TestString", privateKey);
         byte[] decryptedStringArray= decrypter.decrypt(encryptedByteArray, publicKey);
         assertEquals("TestString", new String(decryptedStringArray));
+    }
+
+    @Test
+    public void encryptedStringIsDecryptedCorrectlyWithImplementation() {
+        PublicKeyImpl publicKey = new PublicKeyImpl(new BigInteger("10400312739031"), new BigInteger("21423684981763"));
+        PrivateKeyImpl privateKey = new PrivateKeyImpl(new BigInteger("4860493505831"));
+        BigInteger[] encryptedByteArray = {new BigInteger("3547036742032"),
+                new BigInteger("7533585362812"),
+                new BigInteger("10644284804799") };
+        BigInteger[] decrypted = decrypter.decrypt(encryptedByteArray, privateKey, publicKey);
+        Assertions.assertEquals(new BigInteger("97"), decrypted[0]);
+        Assertions.assertEquals(new BigInteger("98"), decrypted[1]);
+        Assertions.assertEquals(new BigInteger("99"), decrypted[2]);
+
     }
 }
--- project/src/test/java/rsaToolTest/RSAToolTest.java
+++ project/src/test/java/rsaToolTest/RSAToolTest.java
@@ -1,61 +1,37 @@
 package rsaToolTest;
 
-import asciiutil.AsciiUtil;
-import decrypter.Decrypter;
-import encrypter.Encrypter;
-import keygenerators.KeyGenerator;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
-import org.mockito.runners.MockitoJUnitRunner;
 import rsatool.RSATool;
 
-import java.security.PrivateKey;
+import java.math.BigInteger;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.MockitoAnnotations.initMocks;
+import static org.junit.Assert.assertEquals;
 
 
-@RunWith(MockitoJUnitRunner.class)
 public class RSAToolTest {
-    @Mock
-    private Encrypter encrypter;
 
-    @Mock
-    private Decrypter decrypter;
 
-    @Mock
-    private AsciiUtil asciiUtil;
-
-    @Mock
-    private KeyGenerator keyGenerator;
 
     private static final String testString = "testString";
 
     private RSATool rsaTool;
 
-    private PrivateKey testPrivateKey;
 
     @Before
     public void setUp() {
-        initMocks(this);
         this.rsaTool = new RSATool();
+        rsaTool.generateKeys();
     }
 
     @Test
-    public void encryptCallsEncryptor() {
-   /*   rsaTool.encrypt(testString);
-      verify(encrypter).encrypt(anyString(), any(PrivateKeyImpl.class));*/
+    public void rsaToolEncryptsAndDecryptsAGivenString() {
+        BigInteger[] array = rsaTool.encrypt(testString);
+        BigInteger[] decrypted = rsaTool.decrypt(array);
+        assertEquals(testString, rsaTool.cipherToString(decrypted));
     }
 
-    @Test
-    public void decryptCallsDecryptor() {
-       /* rsaTool.decrypt(testString);
-        verify(decrypter).decrypt(any(byte[].class), any(PublicKeyImpl.class));**/
-    }
+
 
 
 
