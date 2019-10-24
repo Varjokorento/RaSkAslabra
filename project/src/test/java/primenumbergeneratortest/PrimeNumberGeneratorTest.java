184aae9259cefc69e6ab1d811d16931db74fc177
Kalle Pusa <kalle.pusa@gmail.com>
2019-10-24 21:20:12 +0300

    Refaktoroin testiä

--- project/src/test/java/primenumbergeneratortest/PrimeNumberGeneratorTest.java
+++ project/src/test/java/primenumbergeneratortest/PrimeNumberGeneratorTest.java
@@ -17,7 +17,7 @@ public class PrimeNumberGeneratorTest {
     public void testMillerRabinPrimality() {
         int errors = 0;
         int totalPrimes = 0;
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000; i++) {
             OwnBigInteger possiblePrime = OwnBigInteger.getLargeRandom();
             BigInteger bi = new BigInteger(possiblePrime.valueOf());
             boolean answer = PrimeNumberGenerator.millerRabinPrimality(possiblePrime, 3);
